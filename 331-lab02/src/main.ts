43bffb45974733a2ccdfeae076efc9806a8ea4de
pakphoom <pakphoom_k@cmu.ac.th>
2025-08-08 10:24:40 +0700

    update analyse

--- 331-lab02/src/main.ts
+++ 331-lab02/src/main.ts
@@ -3,12 +3,12 @@
 import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { inject } from "@vercel/analytics"
-
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+import { inject } from "@vercel/analytics"
 
+inject()
 const app = createApp(App)
 
 app.use(createPinia())
