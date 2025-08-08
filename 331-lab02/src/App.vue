c1fadccbb7ff7667a7480a69e0cb0317353bfb40
pakphoom <pakphoom_k@cmu.ac.th>
2025-08-08 10:21:45 +0700

    add speed

--- 331-lab02/src/App.vue
+++ 331-lab02/src/App.vue
@@ -2,11 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
 
-<template>
+<template><injectSpeedInsights />
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
